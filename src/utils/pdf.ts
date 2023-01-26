c53c4733923b1c5f1b4a41fcc8ee48dee41ec94e
chentian <chentian@kayak.com>
2023-01-26 16:28:28 +0800

    feat: pdf tool rename

--- src/utils/pdf.ts
+++ src/utils/pdf.ts
@@ -2,10 +2,10 @@ import { saveAs } from 'file-saver';
 import type { UploadFile } from 'antd/es/upload/interface';
 import type { WebViewerInstance } from '@pdftron/webviewer';
 
-export default class Office2Pdf {
+export default class PDF {
   private static licenseKey: 'demo:demo@pdftron.com:73b0e0bd01e77b55b3c29607184e8750c2d5e94da67da8f1d0';
 
-  static async toPDFBuffer(instance: WebViewerInstance, file: UploadFile) {
+  static async office2pdf(instance: WebViewerInstance, file: UploadFile) {
     // perform the conversion with no optional parameters
     const buf = await (instance.Core as any).officeToPDFBuffer(file, {
       l: this.licenseKey,
