2b7c517bdbd302deda73dcd46cf801759030b622
chentian <chentian@kayak.com>
2023-02-20 18:52:08 +0800

    feat: 配置修改

--- .umirc.dev.ts
+++ .umirc.dev.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     LICENSE_KEY:
       'demo:demo@pdftron.com:73b0e0bd01e77b55b3c29607184e8750c2d5e94da67da8f1d0',
     BROWSER_FILE: 'https://www.pdfinto.com/file/',
-    API_URL: 'https://www.pdfinto.com/',
+    API_URL: 'https://www.pdfinto.com',
   },
   proxy: {
     '/api': {
